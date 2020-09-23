98db5651cd5e05eddd476199df4823972728b5af
miscing <miscing-public@protonmail.com>
2020-09-23 04:52:42 +0300

    made radio button more visible

--- src/app/toolbars/toolbars.module.ts
+++ src/app/toolbars/toolbars.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AppRoutingModule } from '../app-routing.module';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule  } from '@angular/material/list';
@@ -11,6 +9,7 @@ import { MatMenuModule  } from '@angular/material/menu';
 import { MatSidenavModule  } from '@angular/material/sidenav';
 import { MatRadioModule } from '@angular/material/radio';
 
+import { AppRoutingModule } from '../app-routing.module';
 import { TopToolbarComponent } from './top-toolbar/top-toolbar.component';
 import { DashboardComponent } from './dashboard.component';
 import { LinksComponent } from './links/links.component';
