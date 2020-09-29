461cb901e80d5b31b2d8b84785056fcdb1ab14b4
miscing <miscing-public@protonmail.com>
2020-09-30 00:14:28 +0300

    centered tabs

--- src/app/toolbars/routes-toolbar/routes-toolbar.component.ts
+++ src/app/toolbars/routes-toolbar/routes-toolbar.component.ts
@@ -3,24 +3,22 @@ import { Component, OnInit } from '@angular/core';
 import { AppRoutingModule } from '../../app-routing.module';
 
 @Component({
-  selector: 'app-routes-toolbar',
-  templateUrl: './routes-toolbar.component.html',
-  styleUrls: ['./routes-toolbar.component.scss']
+	selector: 'app-routes-toolbar',
+	templateUrl: './routes-toolbar.component.html',
+	styleUrls: ['./routes-toolbar.component.scss']
 })
 export class RoutesToolbarComponent implements OnInit {
 	paths :string[];
 	activeLink :string;
 
+	constructor(private appRoutingModule :AppRoutingModule) { }
 
-  constructor(private appRoutingModule :AppRoutingModule) { }
-
-  ngOnInit(): void {
+	ngOnInit(): void {
 		this.paths = this.appRoutingModule.routes();
 		this.activeLink = this.paths[0];
-  }
+	}
 
 	capitalize(input: string): string {
 		return input.charAt(0).toUpperCase() + input.slice(1);
 	}
-
 }
