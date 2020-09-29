f133afe0f83732c1a19cb25d4eb7458e49d48f79
miscing <miscing-public@protonmail.com>
2020-09-29 23:42:50 +0300

    home redirect, function to get routes, if handheld toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { HomeComponent } from './home/home.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 const routes: Routes = [
-	// { path: "", redirectTo: "/home", pathMatch: "full" },
 	{ path: "", component: HomeComponent },
+	{ path: "home",  redirectTo: '', pathMatch: 'full' },
 	{ path: "**", component: PagenotfoundComponent }
 ];
 
@@ -14,4 +14,20 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+	paths :string[] = [];
+
+	constructor() {
+		for (let i=0;i<routes.length;i++) {
+			if (routes[i].path == "**" || routes[i].path == "") {
+				continue;
+			}
+			this.paths.push(routes[i].path)
+		}
+	}
+
+	routes() :string[] {
+		return this.paths;
+	}
+}
--- src/app/toolbars/routes-toolbar/routes-toolbar.component.ts
+++ src/app/toolbars/routes-toolbar/routes-toolbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AppRoutingModule } from '../../app-routing.module';
+
 @Component({
   selector: 'app-routes-toolbar',
   templateUrl: './routes-toolbar.component.html',
   styleUrls: ['./routes-toolbar.component.scss']
 })
 export class RoutesToolbarComponent implements OnInit {
+	paths :string[];
 
-  constructor() { }
+  constructor(private appRoutingModule :AppRoutingModule) { }
 
   ngOnInit(): void {
+		this.paths = this.appRoutingModule.routes();
   }
+
+	capitalize(input: string): string {
+		return input.charAt(0).toUpperCase() + input.slice(1);
+	}
 
 }
