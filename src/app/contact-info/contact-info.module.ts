f44ea5fbbc6578ed694dc43b6542c965e8bacbc1
miscing <miscing-public@protonmail.com>
2020-10-01 19:41:23 +0300

    implemented list and relative font-size

--- src/app/contact-info/contact-info.module.ts
+++ src/app/contact-info/contact-info.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 import { ContactInfoComponent } from './contact-info.component';
 import { CompanyInfoComponent } from './company-info/company-info.component';
@@ -14,6 +15,7 @@ import { EmployeesComponent } from './employees/employees.component';
 	exports: [ContactInfoComponent],
 	imports: [
 		MatCardModule,
+		MatListModule,
 		CommonModule
 	]
 })
