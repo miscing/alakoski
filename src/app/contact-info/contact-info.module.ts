fed3b5a16844979dc5851fa3030ec25baab286d1
miscing <miscing-public@protonmail.com>
2020-10-01 16:51:58 +0300

    seperated logic into two components

--- src/app/contact-info/contact-info.module.ts
+++ src/app/contact-info/contact-info.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 
 import { ContactInfoComponent } from './contact-info.component';
+import { CompanyInfoComponent } from './company-info/company-info.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 
 
 @NgModule({
-	declarations: [ContactInfoComponent],
+	declarations: [ContactInfoComponent, CompanyInfoComponent, EmployeesComponent],
 	exports: [ContactInfoComponent],
 	imports: [
 		MatCardModule,
