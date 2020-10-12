968698ec47f3cc343a7d0780eb222bcbe2ec8476
miscing <miscing-public@pm.me>
2020-10-13 00:02:41 +0300

    made bottom toolbar react only on changes of 5 or bigger

--- src/app/toolbars/bottom-toolbar/bottom-toolbar.component.ts
+++ src/app/toolbars/bottom-toolbar/bottom-toolbar.component.ts
@@ -5,7 +5,7 @@ import { CdkScrollable, ScrollDispatcher } from '@angular/cdk/scrolling';
 import itemsJson from './items.json';
 import { Item } from '../item';
 
-const triggerDiff = 120; // cut-off point to hide toolbar
+const triggerDiff = 60; // cut-off point to hide toolbar
 
 @Component({
 	selector: 'app-bottom-toolbar',
@@ -30,14 +30,21 @@ export class BottomToolbarComponent implements AfterViewInit {
 		this.scrollDispatcher.scrolled().subscribe( (cdk: CdkScrollable) => {
 				if ( cdk ) {
 					let offset = cdk.measureScrollOffset("top");
-					if (offset > 50 && offset > this.oldOffset) {
-						this.hide = true;
-					} else {
-						this.hide = false;
+					if ( Math.abs(this.oldOffset - offset) > 5 ) {
+						if (offset > triggerDiff && offset > this.oldOffset) {
+							if (!this.hide) {
+								this.hide = true;
+								this.cdRef.detectChanges();
+							}
+						} else {
+							if (this.hide) {
+								this.hide = false;
+								this.cdRef.detectChanges();
+							}
+						}
+						this.oldOffset = offset;
 					}
-					this.oldOffset = offset;
 				}
-			this.cdRef.detectChanges();
 		});
 	}
 }
