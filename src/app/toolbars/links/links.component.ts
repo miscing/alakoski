6a24d31d6afe36b3e274d4a62ea5bd5b14edcc44
miscing <miscing-public@pm.me>
2020-11-06 23:19:33 +0200

    fixed coloring issue with mail icon

--- src/app/toolbars/links/links.component.ts
+++ src/app/toolbars/links/links.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
+
+import { BreakpointState, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 
 import { ContactInfo } from "../../contact-info/contactinfo";
 import alakoskiCI from '../../contact-info/alakoski-contactinfo.json';
@@ -7,13 +12,32 @@ import itemsJson from './items.json';
 import { Item } from '../item';
 
 @Component({
-  selector: 'app-links',
-  templateUrl: './links.component.html',
-  styleUrls: ['./links.component.scss']
+	selector: 'app-links',
+	templateUrl: './links.component.html',
+	styleUrls: ['./links.component.scss']
 })
 export class LinksComponent {
 	aInfo :ContactInfo = alakoskiCI;
 	items : Item[] = itemsJson;
+	color :Observable<string>;
 
-  constructor() { }
+	constructor(private breakpointObserver: BreakpointObserver) {}
+
+	ngOnInit(): void{
+		this.color = this.breakpointObserver.observe(
+			'(max-width: 1200px)'
+		).pipe(
+			// if handheld return black to color mail icon black on the white background
+			map((result :BreakpointState )=> {
+				 if (result.matches) {
+					 return "black";
+				 } else {
+					 return "";
+				 }
+			})
+		);
+
+		this.color.subscribe( color => { console.log( color ) });
+	}
+
 }
