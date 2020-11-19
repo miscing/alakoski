460d2fb6c58e71f20059dd5e5e63cf74b1791d24
miscing <miscing-public@pm.me>
2020-11-19 18:16:54 +0200

    https://developers.google.com/web/fundamentals/native-hardware/fullscreen/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent implements OnInit {
 	constructor(private themesService : ThemesService) {}
 
 	ngOnInit() {
+		window.scrollTo(0,1);
 		this.theme = this.themesService.getTheme();
 	}
 
